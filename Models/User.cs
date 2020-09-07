90b0c57492fdc49e68fbee5b2d1ef4669958d183
Bartłomiej Leja <bartomiejleja@gmail.com>
2020-09-07 21:54:36 +0200

    Added homeautomationid to claims

---
+++ Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HomeAutomation.IdentityService.Models
+{
+    public class User
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public Guid HomeAutomationGuid { get; set; }
+
+        public User(string login, string password, Guid homeAutomationGuid)
+        {
+            Login = login;
+            Password = password;
+            HomeAutomationGuid = homeAutomationGuid;
+        }
+    }
+}
--- Services/AuthenticationService.cs
+++ Services/AuthenticationService.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.IdentityModel.Tokens;
+﻿using HomeAutomation.IdentityService.Models;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
@@ -10,28 +12,32 @@ namespace HomeAutomation.IdentityService.Services
 {
     public class AuthenticationService: IAuthenticationService
     {
-        private readonly IDictionary _dictionaryCredentials;
+        private readonly Dictionary<string, User> _dictionaryCredentials;
         private static readonly SigningCredentials SigningCredentials = 
             new SigningCredentials(Startup.SecurityKey, SecurityAlgorithms.HmacSha256);
 
         public AuthenticationService()
         {
             //Add id Guid
-            _dictionaryCredentials = new Dictionary<string, string>()
+            _dictionaryCredentials = new Dictionary<string, User>()
             {
-                {"bleja", "test"},
-                {"blejaService", "test"},
-                {"bleja2", "test2"}
+                {
+                    "bleja", 
+                    new User(
+                        "bleja",
+                        "test",
+                        Guid.Parse("bdcd95ec-2dc6-4a7b-90ca-f132a7784b0f"))
+                },
             };
         }
         public bool VerifyIfClientExist(string login, string password)
         {
-            if (!_dictionaryCredentials.Contains(login))
+            if (!_dictionaryCredentials.ContainsKey(login))
             {
                 throw new Exception("User not found");
             }
 
-            if ((string)_dictionaryCredentials[login] != password)
+            if (_dictionaryCredentials[login].Password != password)
             {
                 throw new Exception("Incorrect password");
             }
@@ -40,10 +46,13 @@ namespace HomeAutomation.IdentityService.Services
 
         public async Task<JwtSecurityToken> CreateToken(string login)
         {
+          //  var id = _dictionaryCredentials.First(u => u.Key == login);
             var claim = new Claim(ClaimTypes.Name, login);
-            var claims = new List<Claim> { claim };
+            var homeAutomationId = new Claim("homeAutomationId", 
+                _dictionaryCredentials.First(u => u.Key == login).Value.HomeAutomationGuid.ToString());
+            var claims = new List<Claim> { claim, homeAutomationId };
 
-           return new JwtSecurityToken(
+            return new JwtSecurityToken(
                 "SignalRAuthenticationSample",
                 "SignalRAuthenticationSample",
                 claims,
@@ -54,8 +63,8 @@ namespace HomeAutomation.IdentityService.Services
         public async Task<JwtSecurityToken> CreateServiceToServiceToken(string serviceType, string homeAutomationLocalLightSystemId)
         {
             var serviceTypeClaim = new Claim(ClaimTypes.Name, serviceType);
-            var serviceIdClaim = new Claim(ClaimTypes.PrimarySid, homeAutomationLocalLightSystemId);
-            var claims = new List<Claim> { serviceTypeClaim, serviceIdClaim };
+            var id = new Claim("Id", homeAutomationLocalLightSystemId);
+            var claims = new List<Claim> { serviceTypeClaim, id };
 
             return new JwtSecurityToken(
                  "SignalRAuthenticationSample",
